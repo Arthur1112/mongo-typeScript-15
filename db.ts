10d4dbaa1c346543166dd784b899f61d793307cb
Arthur <arthur_2335@hotmail.com>
2022-05-24 10:29:43 -0400

    converted platforms to TS

--- db.ts
+++ db.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getDb = async () => {
-  const client = new mongodb.MongoClient(process.env.MONGO_URL);
+  const client = new mongodb.MongoClient(process.env.MONGO_URL!); // add the bang to let TS know we will define it later
   await client.connect();
 
   return client.db('zachs-db');
---
+++ platforms.ts
@@ -0,0 +1,22 @@
+import { getDb } from './db';
+
+interface Platform {
+  name: string;
+}
+
+const getCollection = async () => {
+  const db = await getDb();
+  return db.collection<Platform>('platforms'); // inserted a 'generic type', defining the shape of the collection.
+};
+
+export const createPlatform = async (platform: Platform) => {
+  const col = await getCollection();
+  const insertedResults = await col.insertOne(platform);
+  return insertedResults.insertedId;
+};
+
+export const getPlatforms = async () => {
+  const col = await getCollection();
+  const ret = col.find({});
+  return ret.toArray();
+};
